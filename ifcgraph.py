b4a7b16396074b64098b671891612803c013d458
johltn <j.luttun@laposte.net>
2021-03-21 16:41:53 +0100

    Get subgraph

--- ifcgraph.py
+++ ifcgraph.py
@@ -3,7 +3,54 @@ import sys, time
 import networkx as nx
 import matplotlib.pyplot as plt
 import datetime
+import math
 
+class fuzzy_dict(object):
+    def __init__(self, d, eps=1e-5, guid=None):
+        self.d = d
+        self.eps = eps
+        self.guid = guid
+        
+        mod = - int(round(math.log10(eps * 100)))
+        def c(v):
+            if isinstance(v, float):
+                # print "round(%r, %r) = %r" % (v, mod, round(v, mod))
+                return round(v, mod)
+            elif isinstance(v, dict):
+                return tuple(sorted((k,c(v)) for k, v in v.items()))
+            elif isinstance(v, (tuple, list)):
+                return type(v)(map(c, v))
+            else:
+                return v
+                
+        self.h = hash(c(self.d))
+        
+    def __hash__(self):
+        return self.h
+         
+    def __eq__(self, other):
+    
+        def eq(v1, v2):
+            if type(v1) != type(v2):
+                return False
+            if isinstance(v1, float):
+                if abs(v1 - v2) > self.eps: return False
+            elif isinstance(v1, dict):
+                if not (fuzzy_dict(v1, eps=self.eps) == fuzzy_dict(v2, eps=self.eps)): return False
+            elif isinstance(v1, (tuple, list)):
+                if len(v1) != len(v2): return False
+                for a, b in zip(v1, v2):
+                    if not eq(a, b): return False
+            else:
+                if v1 != v2: return False
+            return True
+        
+        if set(self.d.keys()) != set(other.d.keys()): return
+        
+        for v1, v2 in ((self.d[k], other.d[k]) for k in self.d):
+            if not eq(v1, v2): return False
+            
+        return True
 
 
 def add_to_graph(existing, new, G):
@@ -13,22 +60,63 @@ def add_to_graph(existing, new, G):
 def is_entity_instance(val):
     return val.id() != 0 and isinstance(val, ifcopenshell.entity_instance)
 
+def remove_id_key(d):
+    new_d = {}
+    for k, v in d.items():
+        if k != 'id':
+            new_d[k] = v
+
+    return new_d
+    
 def create_graph(ifc_file):
     G = nx.DiGraph()
     for entity in ifc_file:
         info = entity.get_info()
         G.add_node(info['id'], **info)
-        for attribute_value in info.values():
-            if isinstance(attribute_value, ifcopenshell.entity_instance):
-                if attribute_value.id() != 0:
-                    add_to_graph(entity,attribute_value, G)
-            elif isinstance(attribute_value, tuple):
-                for element in attribute_value:
+
+        # import pdb; pdb.set_trace()
+       
+        for attr_name, attr_val in info.items():
+            if isinstance(attr_val, ifcopenshell.entity_instance):
+                if attr_val.id() != 0:
+                    add_to_graph(entity,attr_val, G)
+            elif isinstance(attr_val, tuple):
+                for element in attr_val:
                     if isinstance(element, ifcopenshell.entity_instance):
                         if element.id() != 0:
                             add_to_graph(entity,element, G)
     return G
 
+def get_hashes():
+    hashes = set()
+    for k,v in G.nodes.items():
+        attrs = fuzzy_dict(remove_id_key(v))
+        # print(attrs)
+        hashes.add(attrs)
+        return hashes
+
+def get_subgraph(node, G):
+    neighbors = nx.all_neighbors(G,node) 
+    sg = nx.DiGraph()
+    for n in neighbors:
+        print(n)
+        print(G.has_edge(node, n))
+        if G.has_edge(node, n):
+            sg.add_edge(node, n)
+        else:
+            sg.add_edge(n, node)
+    return sg
+
+def draw_graph(G):
+    pos = nx.spring_layout(G, k=0.8, iterations=60)  # positions for all nodes
+    nn = nx.draw_networkx_nodes(G, pos,nodelist=G.nodes,node_size=500)
+    ne = nx.draw_networkx_edges(G, pos,edgelist=G.edges)
+
+    # nx.draw_networkx_labels(G, pos, labels, font_size=16)
+
+    plt.axis("off")
+    plt.show()
+    
 
 if __name__ == "__main__":
 
@@ -36,19 +124,20 @@ if __name__ == "__main__":
     ifc_file = ifcopenshell.open(fn)
 
     start_time = time.time()
-    G = create_graph(ifc_file)
-
-    l = list(nx.topological_sort(G))
-    
+    G = create_graph(ifc_file)        
     print("--- %s seconds ---" % (time.time() - start_time))
+    
+    sg = get_subgraph(302, G)
 
-    pos = nx.spring_layout(G, k=0.8, iterations=60)  # positions for all nodes
-    nn = nx.draw_networkx_nodes(G, pos,nodelist=G.nodes,node_size=500)
-    ne = nx.draw_networkx_edges(G, pos,nodelist=G.edges)
+    draw_graph(sg)
 
-    # import pdb; pdb.set_trace()
+ 
+    
 
-    #nx.draw_networkx_labels(G, pos, labels, font_size=16)
 
-    plt.axis("off")
-    plt.show()
+
+
+
+
+
+  
